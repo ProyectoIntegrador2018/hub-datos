d8f803c6fbf5628084ccdae00a3203df521301a3
Gerardo Ponce <gerardo_apg_97@hotmail.com>
2020-09-23 17:32:25 -0500

    Creacion API para recibir datos de muestra

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
-const express = require( 'express' );
-const morgan = require( 'morgan' );
-
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
+const data = require("./data/data.json");
 
-app.use( morgan( 'dev' ) );
+app.use(morgan("dev"));
 
-app.get( '/', (req, res) => {
-    res.send( 'Home route' );
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
 });
 
-app.listen( 8080, () => {
-    console.log( 'Servidor corriendo en el puerto 8080' );
+app.get("/api/v1", (req, res) => {
+  res.json(data);
+});
+
+app.listen(8080, () => {
+  console.log("Servidor corriendo en el puerto 8080");
 });
