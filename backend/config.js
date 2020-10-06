f9b34cf3e34b0ca6b274198aa3fe0080a2a3a80b
Iván Muñiz <ivanmr_96@live.com.mx>
2020-10-06 04:34:46 -0500

    Deploy changes

--- backend/config.js
+++ backend/config.js
@@ -4,3 +4,5 @@ module.exports.secret = "suSuperSecret";
 
 // JWT Token
 exports.SECRET_TOKEN = process.env.SECRET_TOKEN || "secret"
+// PORT
+exports.PORT = process.env.PORT || "8000";
--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,13 @@ const app = express();
 require("./db/mongoose");
 const cors = require("./middleware/cors");
 const router = require("./routes/routes");
-
-var port = 8000;
+const { PORT } = require("./config");
 
 app.use(cors);
 app.use(express.json()); // parsea a json
 app.use(morgan("dev"));
 app.use(router);
 
-app.listen(port, () => {
-  console.log("Servidor corriendo en el puerto " + port);
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en el puerto " + PORT);
 });
