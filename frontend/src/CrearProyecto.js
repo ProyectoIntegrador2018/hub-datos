093e19326dff0916f811a16eebef67d154a15c06
emifervi <emifervi@hotmail.com>
2020-10-14 15:25:24 -0700

    Add options for multiple partners, catching state

--- frontend/src/CrearProyecto.js
+++ frontend/src/CrearProyecto.js
@@ -9,8 +9,22 @@ function CrearProyecto() {
   const [description, setDescription] = useState("");
   const [image, setImage] = useState(null);
   const [imgUrl, setImgUrl] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [status, setStatus] = useState("Activo");
+  const [partners, setPartners] = useState([]);
 
-  const _handleChange = (e) => { _handlePreview(e, setImage, setImgUrl) };
+  const _handleChange = (e) => {
+    e.preventDefault();
+    _handlePreview(e, setImage, setImgUrl);
+  };
+
+  const _handlePartners = (e) => {
+    e.preventDefault();
+    const newPartner = e.target.value;
+    const newPartners = [...partners, newPartner];
+    setPartners(newPartners);
+  };
 
   return (
     <CollectionForm
@@ -22,6 +36,14 @@ function CrearProyecto() {
       setDescription={setDescription}
       imgUrl={imgUrl}
       _handleChange={_handleChange}
+      startDate={startDate}
+      setStartDate={setStartDate}
+      endDate={endDate}
+      setEndDate={setEndDate}
+      status={status}
+      setStatus={setStatus}
+      partners={partners}
+      setPartners={_handlePartners}
       variant="Proyecto"
     />
   );
--- frontend/src/components/CollectionForm.js
+++ frontend/src/components/CollectionForm.js
@@ -5,9 +5,10 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Image from "react-bootstrap/Image";
 import placeholder from "../assets/placeholder.png";
-import React from "react";
+import React, { useState } from "react";
 import RoundedButton from "./RoundedButton";
 import Row from "react-bootstrap/Row";
+import { Subbutton } from "./Subbutton";
 
 function CollectionForm({
   title,
@@ -18,8 +19,55 @@ function CollectionForm({
   setAbstract,
   setDescription,
   _handleChange,
+  startDate,
+  setStartDate,
+  endDate,
+  setEndDate,
+  status,
+  setStatus,
+  partners,
+  setPartners,
   variant,
 }) {
+  const options = [
+    "",
+    "Microsoft",
+    "Saphana",
+    "Intel",
+    "Amazon Web Services",
+    "Dell EMC",
+    "Oracle",
+    "IBM Watson",
+    "Cisco",
+    "Tableau",
+    "Cemex",
+    "Heineken",
+    "Chevron",
+    "Arca Continental",
+  ];
+
+  const partnerSelect = (
+    <Form.Control
+      as="select"
+      className="my-2 custom-input"
+      onChange={(e) => {
+        setPartners(e);
+      }}
+    >
+      {options.map((option, i) => (
+        <option key={`option_${i}`}>{option}</option>
+      ))}
+    </Form.Control>
+  );
+
+  const [partnerList, setPartnerList] = useState([partnerSelect]);
+
+  const _handleClick = (e) => {
+    e.preventDefault();
+    let newPartners = [...partnerList, partnerSelect];
+    setPartnerList(newPartners);
+  };
+
   const counterClass = (count, middle, limit) => {
     if (count < middle) {
       return "text-muted";
@@ -38,21 +86,23 @@ function CollectionForm({
             <Form className="ml-auto mr-auto">
               <Form.Group>
                 <h3 className="mb-3">Datos del {variant}</h3>
+                <Form.Label className="font-weight-bold">Title</Form.Label>
                 <Form.Control
                   size="lg"
                   type="text"
-                  placeholder="Title"
                   className="custom-input"
                   onChange={(e) => {
                     setTitle(e.target.value);
                   }}
                   value={title}
                 />
+                <Form.Label className="font-weight-bold mt-4">
+                  Descripción Corta
+                </Form.Label>
                 <Form.Control
                   size="lg"
                   as="textarea"
-                  placeholder="Descripción Corta"
-                  className="mt-4 custom-input"
+                  className="custom-input"
                   maxLength={280}
                   onChange={(e) => {
                     setAbstract(e.target.value);
@@ -62,12 +112,14 @@ function CollectionForm({
                 <Form.Text className={counterClass(abstract.length, 140, 280)}>
                   Characters: {abstract.length}/280
                 </Form.Text>
+                <Form.Label className="font-weight-bold mt-4">
+                  Descripción Detallada
+                </Form.Label>
                 <Form.Control
                   size="lg"
                   as="textarea"
                   rows={5}
-                  placeholder="Descripción Detallada"
-                  className="mt-4 custom-input"
+                  className="custom-input"
                   maxLength={560}
                   onChange={(e) => {
                     setDescription(e.target.value);
@@ -81,46 +133,60 @@ function CollectionForm({
                 </Form.Text>
               </Form.Group>
               <Form.Group>
-                <Form.Control
-                  type="text"
-                  placeholder={`Encargado del ${variant}`}
-                  className="custom-input"
-                  size="lg"
-                />
+                <Form.Label className="font-weight-bold">{`Encargado del ${variant}`}</Form.Label>
+                <Form.Control type="text" className="custom-input" size="lg" />
               </Form.Group>
               <Form.Group>
-                <Form.Label className="pt-3 mr-4">
+                <Form.Label className="pt-3 mr-4 font-weight-bold">
                   Estatus del {variant}:
                 </Form.Label>
-                <Form.Check
-                  inline
-                  label="Activo"
-                  type="radio"
-                  className="pr-5"
-                  size="lg"
-                  name="status"
-                />
-                <Form.Check
-                  inline
-                  label="Finalizado"
-                  type="radio"
-                  className="pl-5"
-                  size="lg"
-                  name="status"
-                />
+                <div onChange={(e) => {setStatus(e.target.value)}}>
+                  <Form.Check
+                    inline
+                    label="Activo"
+                    type="radio"
+                    className="pr-5"
+                    size="lg"
+                    name="status"
+                    value="activo"
+                  />
+                  <Form.Check
+                    inline
+                    label="Finalizado"
+                    type="radio"
+                    className="pl-5"
+                    size="lg"
+                    name="status"
+                    value="finalizado"
+                  />
+                </div>
               </Form.Group>
               <Form.Row className="pt-2">
                 <Form.Group as={Col}>
-                  <Form.Label>
+                  <Form.Label className="font-weight-bold">
                     {variant === "Proyecto"
                       ? `Inicio del ${variant}`
                       : `Fecha del ${variant}`}
                   </Form.Label>
-                  <Form.Control type="date" className="custom-input" />
+                  <Form.Control
+                    type="date"
+                    className="custom-input"
+                    onChange={(e) => {
+                      setStartDate(e.target.value);
+                    }}
+                  />
                 </Form.Group>
                 <Form.Group as={Col}>
-                  <Form.Label>Fin del Proyecto</Form.Label>
-                  <Form.Control type="date" className="custom-input" />
+                  <Form.Label className="font-weight-bold">
+                    Fin del Proyecto
+                  </Form.Label>
+                  <Form.Control
+                    type="date"
+                    className="custom-input"
+                    onChange={(e) => {
+                      setEndDate(e.target.value);
+                    }}
+                  />
                 </Form.Group>
               </Form.Row>
             </Form>
@@ -145,7 +211,16 @@ function CollectionForm({
               />
             </Form.Group>
           </div>
-          <div className="card-shadow p-4 d-flex justify-content-center">
+          <div className="card-shadow p-4 mb-3">
+            <h3 className="mb-2">Partners</h3>
+            <Form.Group>
+              {partnerList.map((partner, i) => (
+                <div key={i}>{partner}</div>
+              ))}
+            </Form.Group>
+            <Subbutton onClick={_handleClick}>Agregar Partner</Subbutton>
+          </div>
+          <div className="card-shadow p-3 d-flex justify-content-center">
             <RoundedButton type="blackBtn">Editar</RoundedButton>
           </div>
         </Col>
