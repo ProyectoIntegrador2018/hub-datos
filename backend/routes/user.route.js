9e47df2cb24752251360f49e371c82bd0dbe1794
Iván Muñiz <ivanmr_96@live.com.mx>
2020-10-26 02:10:03 -0600

    Updated commented documentation

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,14 +6,8 @@ const router = new Router();
 
 /*
  * REGISTER ROUTES
- * GET  /users                      - Returns all registered users
- * POST /users/super_admin          - Create a new user with role super_admin    
- * POST /users/socio_tecnologico    - Create a new user with role socio_tecnologico
- * POST /users/investigador         - Create a new user with role investigador
- * POST /users/alumno               - Create a new user with role alumno
- * POST /users/maestro              - Create a new user with role maestro
- * POST /users/admin                - Create a new user with role admin
- * POST /users/socio_comercial      - Create a new user with role socio_comercial
+ * GET  /users  - Returns all registered users
+ * POST /users  - Create a new user with the specified role in the request.    
  */
 router.get("/", async (req, res) => {
     await userController.getAllUsers(req, res);
@@ -21,13 +15,5 @@ router.get("/", async (req, res) => {
 
 router.post("/", userController.register);
 router.post("/", auth, verifyRole(["super_admin"]), userController.protectedRegister);
-
-// router.post("/super_admin", auth, verifyRole(["super_admin"]), async (req, res) => {
-//     await user.userRegister(req.body, "super_admin", res);
-// });
-
-// router.post("/admin", auth, verifyRole(["super_admin"]), async (req, res) => {
-//     await user.userRegister(req.body, "administrador", res);
-// });
 
 module.exports = router;
