a5b7192bcc43671b6b35054c0b5e4ca0ba0a061d
Iván Muñiz <ivanmr_96@live.com.mx>
2020-10-26 20:45:23 -0600

    Minor changes

--- backend/routes/project.route.js
+++ backend/routes/project.route.js
@@ -6,11 +6,12 @@ const router = new Router();
 
 /*
  * PROJECTS ROUTES
- * GET      /projects           - Returns all the projects
- * POST     /projects           - Creates a new project in the data base.
- * GET      /projects/:id       - Searches a project by ID
- * PUT      /projects/:id       - Edit a project by ID
- * DELETE   /projects/:id       - Delete a project by ID
+ * GET      /projects             - Returns all the projects
+ * POST     /projects             - Creates a new project in the data base.
+ * GET      /projects/:id         - Searches a project by ID
+ * PUT      /projects/:id         - Edit a project by ID
+ * DELETE   /projects/:id         - Delete a project by ID
+ * GET      /projects/my-projects - All projects created by a user
  */
 router.get("/", async (req, res) => {
   await project.getAllProjects(req, res);
